1bc8b45ba5a7a361f3c49bbc8ed031b25e2c232a
ashleyjohnvargas <vashleyjohn@gmail.com>
2024-12-19 20:48:48 +0800

    with_image_preview

--- EcommerceSystem/EcommerceSystem/obj/Debug/net8.0/EcommerceSystem.AssemblyInfo.cs
+++ EcommerceSystem/EcommerceSystem/obj/Debug/net8.0/EcommerceSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304ffebcfeacb655661249b6e7a37f4ddd44be7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88986522fb73cb3bfb3831cdd2e83f499367f3bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
